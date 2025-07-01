5b54ab762d6e10183550837bdaa1f4e95cb66cb4
Nacho <barrojojoaquin2@gmail.com>
2025-06-30 21:38:34 -0300

    agregamos el maquetado del login

--- src/components/pages/producto/Login.jsx
+++ src/components/pages/producto/Login.jsx
@@ -7,45 +7,37 @@ const Login = () => {
       <Row sx={1} md={2}>
         <Col>
           <Form>
-
             <Form.Group className="mb-3" controlId="email">
-
               <Form.Label>Email</Form.Label>
 
               <Form.Control type="email" placeholder="Ej: juanperez@mail.com" />
 
               <Form.Text className="text-danger">
-
                 este es un mensaje de error
-
               </Form.Text>
-
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="password">
-
               <Form.Label>Contraseña</Form.Label>
 
               <Form.Control type="password" placeholder="Contraseña" />
 
-               <Form.Text className="text-danger">
-
+              <Form.Text className="text-danger">
                 este es un mensaje de error
-
               </Form.Text>
-
             </Form.Group>
 
             <Button variant="success" type="submit">
-
               Enviar
-
             </Button>
-
           </Form>
         </Col>
         <Col>
-        <img src="https://images.pexels.com/photos/885021/pexels-photo-885021.jpeg" alt="cafe" className="w-100"/>
+          <img
+            src="https://images.pexels.com/photos/885021/pexels-photo-885021.jpeg"
+            alt="cafe"
+            className="w-100"
+          />
         </Col>
       </Row>
     </section>
