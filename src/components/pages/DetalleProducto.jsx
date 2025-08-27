7ffa69c9c412e97df118843528c166c47d72ea71
Nacho <barrojojoaquin2@gmail.com>
2025-08-27 19:58:08 -0300

    mostramos el detalle del producto seleccionado en la pagina principal

--- src/components/pages/DetalleProducto.jsx
+++ src/components/pages/DetalleProducto.jsx
@@ -1,15 +1,21 @@
 import { Container, Card, Row, Col } from "react-bootstrap";
 import { useParams } from "react-router";
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import { obtenerProductoPorId } from "../../helpers/queries";
 
-const DetalleProducto = ({buscarProducto}) => {
-    const {id} = useParams()
-    const [producto, setProducto]= useState([])
-    useEffect(()=>{
-      const productoBuscado= buscarProducto(id)
-      setProducto(productoBuscado)
-    }, [])
-
+const DetalleProducto = () => {
+  const { id } = useParams();
+  const [producto, setProducto] = useState([]);
+  useEffect(() => {
+    obtenerProducto();
+  }, []);
+  const obtenerProducto = async () => {
+    const respuesta = await obtenerProductoPorId(id);
+    if (respuesta.status === 200) {
+      const productoBuscado = await respuesta.json();
+      setProducto(productoBuscado);
+    }
+  };
   return (
     <Container className="my-3 mainSection">
       <Card>
@@ -18,20 +24,28 @@ const DetalleProducto = ({buscarProducto}) => {
             <Card.Img
               variant="top"
               src={producto.imagen}
-              style={{width:300}}
+              style={{ width: 300 }}
             />
           </Col>
           <Col md={6}>
             <Card.Body>
-              <Card.Title className="primary-font">{producto.nombreProducto}</Card.Title>
+              <Card.Title className="primary-font">
+                {producto.nombreProducto}
+              </Card.Title>
               <hr />
               <Card.Text>
-              {producto.descripcion_amplia}
-              <br/>
-              <br/>
-              <span className="primary-font fw-semibold ">{producto.categoria}:</span> Infusiones
-              <br className='mb-3'/>
-              <span className="primary-font fw-semibold ">${producto.precio}</span></Card.Text>
+                {producto.descripcion_amplia}
+                <br />
+                <br />
+                <span className="primary-font fw-semibold ">
+                  {producto.categoria}:
+                </span>{" "}
+                Infusiones
+                <br className="mb-3" />
+                <span className="primary-font fw-semibold ">
+                  ${producto.precio}
+                </span>
+              </Card.Text>
             </Card.Body>
           </Col>
         </Row>
